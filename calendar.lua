0091eb99ec52a5566b04cddb2fa32194a61ade4d
Fabian Stadler <33486032+stfab@users.noreply.github.com>
2024-09-21 18:11:19 +0200

    Refactor variable names

--- calendar.lua
+++ calendar.lua
@@ -8,36 +8,36 @@ local match_duration = 3
 
 -- function to convert umlauts
 function convert_umlauts(text)
-    text = text:gsub("ä", "ae")
-    text = text:gsub("ö", "oe")
-    text = text:gsub("ü", "ue")
-    text = text:gsub("Ä", "Ae")
-    text = text:gsub("Ö", "Oe")
-    text = text:gsub("Ü", "Ue")
-    text = text:gsub("ß", "ss")
+    text = text:gsub("Ã¤", "ae")
+    text = text:gsub("Ã¶", "oe")
+    text = text:gsub("Ã¼", "ue")
+    text = text:gsub("Ã„", "Ae")
+    text = text:gsub("Ã–", "Oe")
+    text = text:gsub("Ãœ", "Ue")
+    text = text:gsub("ÃŸ", "ss")
     return text
 end
 
 -- function to parse date and time
-function parse_date_time(datum, zeit)
-    local datum_year = string.format("%04d", tonumber(datum:match("[^\p]+%p[^\p]+%p([^\p]+)")))
-    local datum_month = string.format("%02d", tonumber(datum:match("[^\p]+%p([^\p]+)%p[^\p]+")))
-    local datum_day = string.format("%02d", tonumber(datum:match("([^\p]+)%p[^\p]+%p[^\p]+")))
-    local zeit_hour = string.format("%02d", tonumber(zeit:match("(%d+)%p%d+")))
-    local zeit_minute = string.format("%02d", tonumber(zeit:match("%d+%p(%d+)")))
+function parse_date_time(date, time)
+    local date_year = string.format("%04d", tonumber(date:match("[^\p]+%p[^\p]+%p([^\p]+)")))
+    local date_month = string.format("%02d", tonumber(date:match("[^\p]+%p([^\p]+)%p[^\p]+")))
+    local date_day = string.format("%02d", tonumber(date:match("([^\p]+)%p[^\p]+%p[^\p]+")))
+    local time_hour = string.format("%02d", tonumber(time:match("(%d+)%p%d+")))
+    local time_minute = string.format("%02d", tonumber(time:match("%d+%p(%d+)")))
 
     return {
-        year = datum_year,
-        month = datum_month,
-        day = datum_day,
-        hour = zeit_hour,
-        minute = zeit_minute
+        year = date_year,
+        month = date_month,
+        day = date_day,
+        hour = time_hour,
+        minute = time_minute
     }
 end
 
 -- function to create ical event
-function create_ical_event(spieltag, datum, zeit, gastgeber, gast)
-    local date_time = parse_date_time(datum, zeit)
+function create_ical_event(matchday, date, time, host, guest)
+    local date_time = parse_date_time(date, time)
 
     -- calculate end time
     local endtime = string.format("%02d", tonumber(date_time.hour) + match_duration) .. date_time.minute
@@ -48,26 +48,26 @@ function create_ical_event(spieltag, datum, zeit, gastgeber, gast)
         "DTSTAMP:" .. os.date("!%Y%m%dT%H%M%SZ"),
         "DTSTART:" .. date_time.year .. date_time.month .. date_time.day .. "T" .. date_time.hour .. date_time.minute .. "00",
         "DTEND:" .. date_time.year .. date_time.month .. date_time.day .. "T" .. endtime .. "00",
-        "SUMMARY:" .. spieltag .. " " .. gastgeber .. "- " .. gast,
+        "SUMMARY:" .. matchday .. " " .. host .. "- " .. guest,
         "END:VEVENT"
     }
     return table.concat(event, "\n")
 end
 
 -- function to parse the schedule
-function parse_spielplan(spielplan, team_name)
+function parse_schedule(schedule, team_name)
     local events = {}
-    local spieltag = nil
-    for line in spielplan:gmatch("[^\r\n]+") do
-        if line:match("Spieltag") then
-            spieltag = line
+    local matchday = nil
+    for line in schedule:gmatch("[^\r\n]+") do
+        if line:match("Matchday") then
+            matchday = line
         else
-            local spielnummer, datum_zeit, gastgeber, gast = line:match("(%d+)%s+([^\t]+)%s+([^\t]+)%s+([^\t]+)")
-            if gastgeber == team_name or gast == team_name then
-                local datum, zeit = datum_zeit:match("[^\s]%s+([^\s]+)%s%p%s([^\s]+)")
-                gastgeber = convert_umlauts(gastgeber)
-                gast = convert_umlauts(gast)
-                table.insert(events, create_ical_event(spieltag, datum, zeit, gastgeber, gast))
+            local match_number, date_time, host, guest = line:match("(%d+)%s+([^\t]+)%s+([^\t]+)%s+([^\t]+)")
+            if host == team_name or guest == team_name then
+                local date, time = date_time:match("[^\s]%s+([^\s]+)%s%p%s([^\s]+)")
+                host = convert_umlauts(host)
+                guest = convert_umlauts(guest)
+                table.insert(events, create_ical_event(matchday, date, time, host, guest))
             end
         end
     end
@@ -78,14 +78,14 @@ end
 function main()
     local file = io.open(file_name .. ".txt", "r")
     if not file then
-        print("Fehler beim Öffnen der Datei: " .. file_name .. ".txt")
+        print("Error opening file: " .. file_name .. ".txt")
         return
     end
 
-    local spielplan = file:read("*all")
+    local schedule = file:read("*all")
     file:close()
 
-    local events = parse_spielplan(spielplan, team_name .. " ") -- empty space is necessary
+    local events = parse_schedule(schedule, team_name .. " ") -- empty space is necessary
 
     local ical = {
         "BEGIN:VCALENDAR",
@@ -100,7 +100,7 @@ function main()
     ical_file:write(table.concat(ical, "\n"))
     ical_file:close()
 
-    print("Kalenderdatei wurde erstellt: " .. file_name .. ".ics")
+    print("Calendar file created: " .. file_name .. ".ics")
 end
 
 main()
